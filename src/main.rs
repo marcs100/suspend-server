e5fb91a1f6e3ec36d06d730ac4796f10ccae5573
marcs100 <marcs100@gmail.com>
2024-12-29 13:26:00 +0000

    Added logging

--- src/main.rs
+++ src/main.rs
@@ -12,24 +12,32 @@ fn main() {
     //let progs_check = ["brave", "borg", "borgmatic"]; //put this in a config file
     let mut suspend_ok: bool = false;
     const MAX_TRIES: i32 = 10;
+    const CONFIG_FILE: &str = "suspend-server.conf";
     let mut tries: i32 = 0;
 
     //config file
     let mut conf = config::ConfigFile::default(); //get the config
-    conf.get_config("/etc/");
+    conf.get_config("/etc/"); //this function should be modified to return Result<String>
+
+    let mut log_path: String= conf.log_path.clone();
+    log_path.push_str(CONFIG_FILE);
+    let log_file: &str = log_path.as_str();
+
 
     if conf.programs_list.len() == 0{
+        logger(log_file,"Error in config file - no programs to check");
         println!("Error in config file - no programs to check");
         return;
     }
 
     let progs_iter = conf.programs_list.split(","); //get iterator for programs to check
     let progs = progs_iter.collect::<Vec<_>>();
-    println!("progs = {:?}", progs);
+    //println!("progs = {:?}", progs);
     while (tries < MAX_TRIES) && !suspend_ok{
         tries += 1;
         suspend_ok=true;
         for prog in progs.iter(){
+            logger(log_file, format!("Checking for {}",prog).as_str());
             println!("Checking for {}", prog);
             let mut sh_command = Command::new("pgrep");
             sh_command.arg(prog);
@@ -37,6 +45,7 @@ fn main() {
             let progs = String::from_utf8(sh_output.stdout).unwrap(); // to do - handle this error
             //println!("Programs detected: {}",progs);
             if progs.len() > 0{
+                logger(log_file, format!("{} is running, can't suspend", prog).as_str());
                 println!("{} is running, can't suspend", prog);
                 suspend_ok = false;
                 //sleep for 15 mins
@@ -48,9 +57,11 @@ fn main() {
     }
 
     if suspend_ok{
+        logger(log_file, "systemctl suspend");
         println!("systemctl suspend");
     }
     else{
+        logger(log_file, "Conditions not met to suspend system");
         println!("Conditions not met to suspend system");
     }
 
